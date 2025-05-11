a4c2facafce1ecedf8bd38115ab88e7d8b3c2ef9
Tatiana Vishnyakova <akawildcat@gmail.com>
2025-05-11 15:26:08 +0200

    changed the Migrations folder

--- CollectionBack/Infrastructure/Migrations/20250511132513_InitialCreate.cs
+++ CollectionBack/Infrastructure/Migrations/20250511132513_InitialCreate.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace CollectionBasic.Migrations
+namespace CollectionBasic.Infrastructure.Migrations
 {
     /// <inheritdoc />
     public partial class InitialCreate : Migration
